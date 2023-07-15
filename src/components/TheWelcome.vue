6c8bff6ccb7210718157edb1640e85691c8182bd
BigBang <bigbang@BigBangs-MacBook-Pro.local>
2023-07-15 10:30:32 -0500

    voice recognition

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import VoiceRecognition from './VoiceRecognition.vue';
+import CommunityIcon from './icons/IconCommunity.vue'
+</script>
+
+<template>
+  <div>
+    <WelcomeItem>
+      <template #icon>
+        <CommunityIcon />
+      </template>
+      <template #heading>Say something</template>
+
+      <VoiceRecognition />
+    </WelcomeItem>
+  </div>
+</template>
